2327ae9811864f178ef496d450fc88f3ef78f734
Guido Perman <guidoyaelperman@gmail.com>
2025-02-25 12:22:01 -0300

    add gp-side-menu integration and update admin layout component

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
+import { ApxSideMenuComponent, TitleColor } from 'gp-side-menu';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, SideMenuComponent, ApxSideMenuComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css',
 })
 export class AdminLayoutComponent {
   isAuthenticated = signal(false);
+  TitleColor = TitleColor;
 
   onSignOut() {
     this.isAuthenticated.set(false);
